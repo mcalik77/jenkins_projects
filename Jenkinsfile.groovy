2cf803effe5f2f6946b6c555ef2a1406d724170b
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-28 16:53:28 -0700

    added

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,24 @@
+node{
+    stage("Uptade jenkins"){
+        properties([parameters([string(defaultValue: '52.212.111.79', description: 'Please provide IP', name: 'Environment', trim: true)])])
+        sh "echo Parameter added"
+    }
+    stage("Install git"){
+        sh "ssh ec2-user@${Environment} sudo yum install git -y"
+    }
+    stage("Pull Repo"){
+        sh "ssh ec2-user@${Environment} git clone https://github.com/miguelgrinberg/flask-examples.git"
+    }
+    stage("Install Requirements"){
+        //sh "virtualenv /tmp/venv"
+        //sh ". /tmp/venv/bin/activate"
+        sh "echo Hello"
+        
+    }
+    stage("Pip Install"){
+        sh "ssh ec2-user@${Environment} pip install -r ~/flask-examples/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh ec2-user@${Environment} python ~/flask-examples/01-hello-world/hello.py"
+    }
+}
