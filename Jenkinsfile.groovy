b0dbffb9362f5f279c75c2e7d133a0cd9384dc32
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-28 16:54:01 -0700

    added python

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,7 +4,7 @@ node{
         sh "echo Parameter added"
     }
     stage("Install git"){
-        sh "ssh ec2-user@${Environment} sudo yum install git -y"
+        sh "ssh ec2-user@${Environment} sudo yum install git python-pip -y"
     }
     stage("Pull Repo"){
         sh "ssh ec2-user@${Environment} git clone https://github.com/miguelgrinberg/flask-examples.git"
