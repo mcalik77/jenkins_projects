c82eae59951a9461dd8af589adab84ad84a2911a
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-28 17:44:08 -0700

    edited

---
+++ Flaskex/Jenkinsfile.groovy
@@ -0,0 +1,27 @@
+node{
+    stage("Uptade jenkins"){
+        properties([parameters([string(defaultValue: '52.212.111.79', description: 'Please provide IP', name: 'ENVIR', trim: true)])])
+        sh "echo Parameter added"
+    }
+    stage("Install git"){
+        sh "ssh ec2-user@${ENVIR} sudo yum install git python-pip -y"
+    }
+    stage("Remove repo"){
+        //sh  "ssh ec2-user@${ENVIR} sudo rm -rf /home/ec2-user/flask-examples"
+    }
+    stage("Pull Repo"){
+        sh "ssh ec2-user@${ENVIR} git clone https://github.com/anfederico/Flaskex.git"
+    }
+    stage("Install Requirements"){
+        //sh "virtualenv /tmp/venv"
+        //sh ". /tmp/venv/bin/activate"
+        sh "echo Hello"
+        
+    }
+    stage("Pip Install"){
+        sh "ssh ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/Flaskex/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh ec2-user@${ENVIR} python   /home/ec2-user/Flaskex/app.py"
+    }
+}
