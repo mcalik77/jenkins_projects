bf67278b982039c84171809df5123ce8dde8dcc1
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-28 18:11:38 -0700

    add new app

---
+++ Storm/Jenkinsfile.groovy
@@ -0,0 +1,21 @@
+node{
+    stage("Uptade jenkins"){
+        properties([parameters([string(defaultValue: '52.212.111.79', description: 'Please provide IP', name: 'ENVIR', trim: true)])])
+        sh "echo Parameter added"
+    }
+    stage("Install git"){
+        sh "ssh ec2-user@${ENVIR} sudo yum install git python-pip -y"
+    }
+    stage("Pull Repo"){
+        sh "ssh ec2-user@${ENVIR} git clone https://github.com/mcalik77/stormpath-flask-sample.git 2> /dev/null"
+    }
+    stage("Install Requirements"){
+        sh "echo Hello"      
+    }
+    stage("Pip Install"){
+        sh "ssh ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/stormpath-flask-sample/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh ec2-user@${ENVIR} python   /home/ec2-user/stormpath-flask-sample/bootstrap.py"
+    }
+}
