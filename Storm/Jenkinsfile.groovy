ea183eaffc534fac90a566bcbcc7ca3ecf7111d6
Mustafa Calik <mcalikdevop@gmail.com>
2019-08-28 18:25:42 -0700

    edited

--- Storm/Jenkinsfile.groovy
+++ Storm/Jenkinsfile.groovy
@@ -6,6 +6,12 @@ node{
     stage("Install git"){
         sh "ssh ec2-user@${ENVIR} sudo yum install git python-pip -y"
     }
+     stage("Remove repo"){
+        sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/stormpath-flask-sample"
+    }
+     stage("Remove PID"){
+        sh "ssh  ec2-user@${ENVIR} sudo kill $(sudo lsof -i:5000   | awk '{print $2}' | grep [[:digit:]]) 2> /dev/null"
+    }
     stage("Pull Repo"){
         sh "ssh ec2-user@${ENVIR} git clone https://github.com/mcalik77/stormpath-flask-sample.git 2> /dev/null"
     }
